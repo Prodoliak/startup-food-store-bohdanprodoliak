d40f2ef4342a86e9f69574af82187e22159eda87
Prodoliak <prodoliak@gmail.com>
2024-02-29 18:56:10 +0200

    add pages

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
-
-$(".burger-wrap").click(function () {
-    $(".header_wrap").toggleClass("header_wrap--open");
-    $(".burger").toggleClass('burger--close');
+$('.slider_init').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
 });
-$(".main_sidebar-burger").click(function () {
-    $(".main_sidebar").toggleClass("main_sidebar--open");
-    $(".main_sidebar-burger").toggleClass('main_sidebar-burger--open');
-});
+		
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -15,4 +14,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         behavior: 'smooth'
       });
     });
+});
+
+$(".burger-wrap").click(function () {
+    $(".header_nav").toggleClass("header_nav--open");
+    $(".burger").toggleClass('burger--close');
+});
+$(".main_sidebar-burger").click(function () {
+    $(".main_sidebar").toggleClass("main_sidebar--open");
+    $(".main_sidebar-burger").toggleClass('main_sidebar-burger--open');
 });
